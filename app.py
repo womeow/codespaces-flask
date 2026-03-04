470118e6b55593c891ee0bb96326337ce27e6b9f
womeow <119796064+womeow@users.noreply.github.com>
2026-03-04 11:43:50 +0000

    loshara

--- app.py
+++ app.py
@@ -65,6 +65,40 @@ def delete_task(task_id):
 def home():
     return render_template("index.html")
 
+from datetime import datetime
+
+# обработка GET-запроса
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    # сортировка: сначала по приоритету (1-высокий), затем по дедлайну
+    sorted_tasks = sorted(tasks, key=lambda x: (x.get('priority', 3), x.get('deadline', '9999-12-31')))
+    return jsonify(sorted_tasks)
+
+# добавление новой задачи
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    global next_id
+    data = request.get_json() # получаем данные, которые прислал браузер
+    if not data or "title" not in data or not data["title"].strip():
+        return jsonify({"error": "Title is required"}), 400
+    
+    # создаем словарь (объект) задачи
+    task = {
+        "id": next_id,
+        "title": data["title"].strip(),
+        "completed": False,
+        "priority": int(data.get("priority", 3)), # записываем важность (по умолчанию 3 - низкая)
+        "deadline": data.get("deadline") # записываем дату (если она есть)
+    
+    }
+    # добавляем в общий список
+    tasks.append(task)
+    next_id += 1
+    save_tasks() # записываем в файл tasks.json
+    return jsonify(task), 201
+ #дальше сохранение и ответ сервера
+
+
 # ----------------- Run -----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
